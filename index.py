de9c95a2120894777b00a3fb6d2c15686d5acaf9
jufragarcaCUN <juan_garnicac@cun.edu.co>
2026-07-08 07:57:01 -0500

    Actualizacion de cambios y videos

--- index.py
+++ index.py
@@ -1,14 +1,13 @@
 import streamlit as st
 
-# 1. Configuración de la página principal
-st.set_page_config(page_title="Menú de Presentaciones", page_icon="📊")
+st.set_page_config(page_title="Menú de Presentaciones", page_icon="📊", layout="wide")
 
-# 2. Definimos las páginas apuntando directo a tus archivos en la misma carpeta
 paginas = [
     st.Page("presentacion.py", title="Presentación Principal", icon="📊"),
     st.Page("Objeciones.py", title="Módulo de Objeciones", icon="🎯"),
+    st.Page("cambios.py", title="Cambios Modelo de Objeciones", icon="🔄"),
+    st.Page("videos.py", title="Rendimiento AWS & Videos", icon="⚙️"),
 ]
 
-# 3. Inicializamos la navegación nativa
 pg = st.navigation(paginas)
 pg.run()
